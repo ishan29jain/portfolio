ca262e9cb1f1c88da7073092b081113d22e6869f
ishan29jain <84621870+ishan29jain@users.noreply.github.com>
2024-04-12 01:26:07 +0530

    Add files via upload
    Uploaded website codes.

---
+++ script.js
@@ -0,0 +1,19 @@
+var tablinks = document.getElementsByClassName("tab-links")
+var tabcontents = document.getElementsByClassName("tab-contents")
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+var typed = new Typed('#element', {
+    strings: ['Web Developer.', 'Graphic Designer.', 'Video Editor.'],
+    typeSpeed: 100,
+});
